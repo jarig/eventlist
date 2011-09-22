6a5fae3bd82393830ff5db0b777bd8b75720405b
unknown <jarik@.(none)>
2011-09-23 01:50:43 +0300

    new files

---
+++ party/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Party(models.Model):
+    ROLES = (
+        (u'O',u'organizer'),
+        (u'P',u'participant'),
+    )
+    participant = models.ManyToManyField(User, through='Participation')
+    role = models.CharField(max_length=1, choices=ROLES, default='P')
+    
+    pass #party model
+
+
+class Participation(models.Model):
+    party = models.ForeignKey(Party)
+    user = models.ForeignKey(User)
+    
+    dateAdded = models.DateTimeField(auto_now_add=True)
+    
+    pass
