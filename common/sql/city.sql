6ac3da7de1c4c530e679c1840bb3524df2bb32b2
jarig <gjazik@gmail.com>
2011-11-19 03:01:44 +0200

    daily

---
+++ common/sql/city.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO `common_city`(`country`, `name`) VALUES (68, 'Tallinn');
+INSERT INTO `common_city`(`country`, `name`) VALUES (68, 'Tartu');
+INSERT INTO `common_city`(`country`, `name`) VALUES (68, 'Pärnu');
