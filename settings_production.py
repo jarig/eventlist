8ee24846312a0324cf0896c7227c7624e4b4c4b1
jarig <gjazik@gmail.com>
2012-02-14 22:53:03 +0200

    daily

--- settings_production.py
+++ settings_production.py
@@ -8,8 +8,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'test',                      # Or path to database file if using sqlite3.
-        'USER': 'admin',                      # Not used with sqlite3.
-        'PASSWORD': 'eiYaek5Aug',                  # Not used with sqlite3.
+        'USER': 'fbdamp102',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
