cfcea7238a51f81d7d6bd24746d06795d425c03c
jarig <gjazik@gmail.com>
2013-04-23 01:45:41 +0300

    daily

--- search/static/js/search.js
+++ search/static/js/search.js
@@ -1,6 +1,30 @@
 
-
 var Search =
 {
-    
+    options: {
+      id: "fastSearch"
+    },
+    init: function( options )
+    {
+        if ( typeof options != "undefined")
+            Search.options = $.extend(Search.options, options);
+        $(".criteria a","#"+Search.options["id"]+" .chosenSearchCriteria").click(function()
+        {
+            var criteriaName = $(this).attr("action-data");
+            console.log("Remove criteria:" + criteriaName);
+        });
+    },
+    addCriteria: function(name, value)
+    {
+        var tag = $("[name='"+name+"']", "#"+Search.options["id"]);
+        if ( tag[0].tagName.toLowerCase() == "select")
+        {
+            console.log($("option[value='"+value+"'",$(tag)));
+            $("option[value='"+value+"']",$(tag)).attr("selected", "selected");
+        }
+    },
+    run: function()
+    {
+        $("#"+Search.options["id"]).submit();//submit form
+    }
 };
